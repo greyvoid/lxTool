2ca3b4694dc5fcb3110f6039d776aaa016d92249
leishengxie <654380976@qq.com>
2019-10-25 18:32:09 +0800

    aaa

--- QHttpQuickClickTest/main.cpp
+++ QHttpQuickClickTest/main.cpp
@@ -4,11 +4,11 @@
 
 int main(int argc, char *argv[])
 {
-    QNetworkProxy xy;
-    xy.setHostName("127.0.0.1");
-    xy.setPort(8888);
-    xy.setType(QNetworkProxy::HttpProxy);
-    QNetworkProxy::setApplicationProxy(xy);
+//    QNetworkProxy xy;
+//    xy.setHostName("127.0.0.1");
+//    xy.setPort(8888);
+//    xy.setType(QNetworkProxy::HttpProxy);
+//    QNetworkProxy::setApplicationProxy(xy);
 
     QApplication a(argc, argv);
     MainWindow w;
